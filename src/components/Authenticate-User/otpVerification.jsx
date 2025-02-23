bf11c40aa986e8ec6683744e74056462e73d36a9
challavenkataramana <challavenkataramana@gmail.com>
2025-02-23 12:19:18 +0530

    refactor: clean up code formatting and remove unused StepHeader component

--- src/components/Authenticate-User/otpVerification.jsx
+++ src/components/Authenticate-User/otpVerification.jsx
@@ -34,7 +34,7 @@ const OtpVerification = ({ email, onOtpVerified }) => {
     const response = await fetch("http://localhost:5000/api/auth/verify-otp", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, otp:finalOtp }),
+      body: JSON.stringify({ email, otp: finalOtp }),
     });
 
     const data = await response.json();
--- src/components/Authenticate-User/registerform.jsx
+++ src/components/Authenticate-User/registerform.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck,faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const RegisterForm = ({ email }) => {
   const [password, setPassword] = useState("");
